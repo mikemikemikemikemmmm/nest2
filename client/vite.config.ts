9ac888a81f99cf02e265fab0e075f252342dc168
mike <mikem40099@gmail.com>
2023-02-28 20:22:55 +0800

    modify vite confing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'build') {
+    return {
+      base: '/nest2/client/',
+      plugins: [react()],
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
--- staff/vite.config.ts
+++ staff/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'build') {
+    return {
+      base: '/nest2/staff/',
+      plugins: [react()],
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
