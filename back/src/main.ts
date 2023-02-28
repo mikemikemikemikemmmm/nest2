9e620c3ed88bdb3a74adf7f4f2fb5875a7c62fb1
mike <mikem40099@gmail.com>
2023-03-01 00:15:14 +0800

    add port in back env

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // app.use()
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
