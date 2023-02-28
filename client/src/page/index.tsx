7def2fcdb1986c076d728b2cbefe3bd8c6ca95e1
mike <mikem40099@gmail.com>
2023-02-28 21:52:54 +0800

    add carousel 3 jpg

--- client/src/page/index.tsx
+++ client/src/page/index.tsx
@@ -6,7 +6,7 @@ import { INDEX_CAROUSEL_IMG } from '../style/const';
 import carouselImg0 from '/carousel/0.jpg'
 import carouselImg1 from '/carousel/1.jpg'
 import carouselImg2 from '/carousel/2.jpg'
-import carouselImg3 from '/carousel/0.jpg'
+import carouselImg3 from '/carousel/3.jpg'
 export const IndexPage = () => {
     return (
         <PageContainer>
